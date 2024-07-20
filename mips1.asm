bde1218140852170e43483fa75c28acf3497b3ed
Mateus Alves <153127373+suco-mateusalves@users.noreply.github.com>
2024-07-20 16:16:50 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -1,25 +1,25 @@
 .data
-menu:       .asciiz "\n1 - Fahrenheit -> Celsius\n2 - Fibonacci\n3 - Enésimo par\n4 - Sair\nEscolha uma opcao: "
-fahrenheit: .asciiz "Digite a temperatura em Fahrenheit: "
-n_prompt:   .asciiz "Digite o valor de N: "
-result:     .asciiz "Resultado: "
-newline:    .asciiz "\n"
+menu:       	.asciiz "\n1 - Fahrenheit -> Celsius\n2 - Fibonacci\n3 - Enésimo par\n4 - Sair\nEscolha uma opcao: "
+fahrenheit: 	.asciiz "Digite a temperatura em Fahrenheit: "
+entrada_int:	.asciiz "Digite o valor de N: "
+resultado:     	.asciiz "Resultado: "
+newline:    	.asciiz "\n"
 
 .text
 main:
-    li $v0, 4             # syscall para imprimir string
+    li $v0, 4             # chamada para imprimir string
     la $a0, menu          # carregar o endereço da string do menu
     syscall
 
-    li $v0, 5             # syscall para ler um inteiro
+    li $v0, 5             # chamada para ler um inteiro
     syscall
     move $t0, $v0         # mover o valor lido para $t0
 
     beq $t0, 1, fahrenheit_to_celsius
     beq $t0, 2, fibonacci
     beq $t0, 3, enesimo_par
-    beq $t0, 4, exit
-    j main                # caso contrário, repete o menu
+    beq $t0, 4, saida
+    j main                # 'loop' para caso não seja escolhido alguma opção repetir o menu
 
 fahrenheit_to_celsius:
     li $v0, 4             # syscall para imprimir string
@@ -32,7 +32,7 @@ fahrenheit_to_celsius:
 
     sub $t1, $t1, 32      # F - 32
     mul $t1, $t1, 5       # (F - 32) * 5
-    div $t1, 9            # ((F - 32) * 5) / 9
+    div $t1, $t1, 9       # ((F - 32) * 5) / 9
     mflo $t1              # mover o quociente de $lo para $t1
     move $a0, $t1         # mover o resultado para $a0 para impressão
 
@@ -42,7 +42,7 @@ fahrenheit_to_celsius:
 
 fibonacci:
     li $v0, 4             # syscall para imprimir string
-    la $a0, n_prompt      # carregar o endereço da string de prompt de N
+    la $a0, entrada_int   # carregar a string para entrada do inteiro
     syscall
 
     li $v0, 5             # syscall para ler um inteiro
@@ -69,7 +69,7 @@ fibonacci_done:
 
 enesimo_par:
     li $v0, 4             # syscall para imprimir string
-    la $a0, n_prompt      # carregar o endereço da string de prompt de N
+    la $a0, entrada_int   # carregar a string para entrada do inteiro
     syscall
 
     li $v0, 5             # syscall para ler um inteiro
@@ -83,6 +83,6 @@ enesimo_par:
     syscall
     j main                # voltar ao menu
 
-exit:
+saida:
     li $v0, 10            # syscall para sair
     syscall
